4f7ff1746ac90e10e3284bc19293031ded22ff41
Austin-butz <abutz@fandm.edu>
2020-10-01 23:53:06 -0400

    initial commit

---
+++ rbt.h
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+#ifndef RBT_H
+#define RBT_H
+
+struct Node {
+  int data;
+  Node *parent;
+  Node *left;
+  Node *right;
+  int color;
+};
+
+class RedBlackTree {
+    public: 
+        RedBlackTree();
+
+        Node* create_node(Node* parent, const int& entry);
+
+        string ToInfixString() {string output; return ToInfixStringp(root, output); };
+        
+        string ToPrefixString() {string output; return ToPrefixStringp(root, output); };
+        
+        string ToPostfixString() {string output; return ToPostfixStringp(root, output); };
+
+        void preorder(Node* node);
+
+        void inorder(Node* node);
+
+        void postorder(Node* node);
+
+        size_t size() {return numItems;}
+
+        void printtreehelper(Node* node, size_t depth);
+
+        void printTree();
+
+        void rightrotate(Node* input);
+
+        void leftrotate(Node* input);
+
+        void sorttree(Node* input);
+
+        int GetMin();
+
+        int GetMax();
+
+        void insert(int Target);
+    
+    private:
+        unsigned long long int numItems;
+        Node* root;
+        Node* nullnode;
+
+        string ToPostfixStringp(Node* node, string &output);
+
+        string ToPrefixStringp(Node* node, string &output);
+
+        string ToInfixStringp(Node* node, string &output);
+};
+
+#endif
